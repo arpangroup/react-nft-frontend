c8585afc5aae706d1647d2e8268f9b2e3bfe36e1
arpan jana <arpangroup1@gmail.com>
2025-08-12 23:40:55 +0530

    make auto logout when token expire

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Navbar,NavbarV2,Footer} from './components'
+import {Navbar, NavbarV2, Footer} from './components'
 import {Home,Profile,Item, Create,Login,Register, Settings, Forgot, Level, UserDashboardPage, MLMTree} from './pages'
 import { Routes, Route, useLocation } from "react-router-dom";
 import ItemList from './pages/demo/ItemList';
@@ -10,6 +10,7 @@ import UserDetails from './pages/user/UserDetails';
 import Reservation from './pages/reservation/Reservation';
 import DepositPage from './pages/deposit/DepositPage';
 import NotificationList from './pages/notifications/NotificationList';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   const location = useLocation();
@@ -23,33 +24,39 @@ function App() {
       {/* Apply top margin only if Navbar is visible */}
        <div style={{ marginTop: showNavbar ? '60px' : '0' }}>
         <Routes>
+          {/* Public routes */}
           <Route path="/" element={<Home />} />
-          <Route path="/stakes" element={<Stake />} />
-          <Route path="/earn" element={<Earn />} />
-          <Route path=":item/:id" element={<Item />} />
-          <Route path="/userDetails/:id" element={<UserDetails />} />
-
-          
-          <Route path="/reservation" element={<Reservation />} />
-
-          <Route path="/create" element={<Create /> } />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/login" element={ <Login />} />
-          <Route path="/register" element={ <Register />} />
-          <Route path="/forgot" element={ <Forgot />} />
-          <Route path="/settings" element={<Settings />} />
-
-
-          <Route path="/deposit" element={<DepositPage />} />
-
-
-          <Route path="/level" element={<Level />} />
-          <Route path="/dashboard" element={<UserDashboardPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
           <Route path="/tree" element={<MLMTree />} />
 
-          
-          <Route path="/alerts" element={<NotificationList />} />
 
+          {/* Protected routes grouped under ProtectedRoute */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/stakes" element={<Stake />} />
+            <Route path="/earn" element={<Earn />} />
+            <Route path=":item/:id" element={<Item />} />
+            <Route path="/userDetails/:id" element={<UserDetails />} />
+
+            <Route path="/reservation" element={<Reservation />} />
+            
+            <Route path="/create" element={<Create /> } />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/login" element={ <Login />} />
+            <Route path="/register" element={ <Register />} />
+            <Route path="/forgot" element={ <Forgot />} />
+            <Route path="/settings" element={<Settings />} />
+
+            <Route path="/deposit" element={<DepositPage />} />
+
+            <Route path="/level" element={<Level />} />
+            <Route path="/dashboard" element={<UserDashboardPage />} />
+            <Route path="/tree" element={<MLMTree />} />
+            
+            <Route path="/alerts" element={<NotificationList />} />
+
+          </Route>         
         </Routes>
        </div>
        {/* <Footer/> */}
